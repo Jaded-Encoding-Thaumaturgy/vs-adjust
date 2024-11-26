f1561d64fc0ebf431cbdcb00643ace5aae47eecc
emotion3459 <176516814+emotion3459@users.noreply.github.com>
2024-11-26 16:43:31 -0500

    colorspace_conversion: force fp32 bitdepth

--- vsadjust/colorspace.py
+++ vsadjust/colorspace.py
@@ -1,8 +1,9 @@
 from typing import Any
 
 from vskernels import Point
-from vstools import (ColorRange, ColorRangeT, DitherType, Matrix, MatrixT,
-                     Primaries, PrimariesT, Transfer, TransferT, vs)
+from vstools import (
+    FunctionUtil, ColorRange, ColorRangeT, DitherType, Matrix, MatrixT, Primaries, PrimariesT, Transfer, TransferT, vs
+)
 
 __all__ = [
     'colorspace_conversion'
@@ -17,12 +18,14 @@ def colorspace_conversion(
         primaries_in: PrimariesT | None = None, color_range_in: ColorRangeT | None = None,
         dither_type: DitherType = DitherType.AUTO,
 ) -> vs.VideoNode:
+    
+    func = FunctionUtil(clip, colorspace_conversion, bitdepth=32)
 
     resample_kwargs = dict[str, Any](dither_type=dither_type)
 
     if matrix is not None:
         if matrix_in is not None:
-            clip = Matrix.from_param(matrix_in).apply(clip)
+            func.work_clip = Matrix.from_param(matrix_in).apply(func.work_clip)
 
         matrix = Matrix.from_param(matrix, colorspace_conversion)
 
@@ -30,7 +33,7 @@ def colorspace_conversion(
 
     if transfer is not None:
         if transfer_in is not None:
-            clip = Transfer.from_param(transfer_in).apply(clip)
+            func.work_clip = Transfer.from_param(transfer_in).apply(func.work_clip)
 
         transfer = Transfer.from_param(transfer, colorspace_conversion)
 
@@ -38,7 +41,7 @@ def colorspace_conversion(
 
     if primaries is not None:
         if primaries_in is not None:
-            clip = Primaries.from_param(primaries_in).apply(clip)
+            func.work_clip = Primaries.from_param(primaries_in).apply(func.work_clip)
 
         primaries = Primaries.from_param(primaries, colorspace_conversion)
 
@@ -46,10 +49,11 @@ def colorspace_conversion(
 
     if color_range is not None:
         if color_range_in is not None:
-            clip = ColorRange.from_param(color_range_in).apply(clip)
+            func.work_clip = ColorRange.from_param(color_range_in).apply(func.work_clip)
 
         color_range = ColorRange.from_param(color_range, colorspace_conversion)
 
         resample_kwargs |= dict(range=color_range.value_zimg)
 
-    return Point.resample(clip, clip, **resample_kwargs)
+    converted = Point.resample(func.work_clip, func.work_clip, **resample_kwargs)
+    return func.return_clip(converted)
